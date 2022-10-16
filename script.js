2f1f5be495f500c983a46745d04ed674aa662c36
Muhamamd Hamza <110340023+MHamza14@users.noreply.github.com>
2022-10-16 21:46:33 +0500

    Done

--- script.js
+++ script.js
@@ -5,40 +5,43 @@ let getLiveWeather = () => {
             console.log("Getting Response");
             console.log(response.data);
 
-            // let iconcode = response.data.weather[0].icon;
-            // let iconurl = "http://openweathermap.org/img/wn/" + iconcode + ".png";
+            let date = new Date(response.data.dt * 1000); 
+            let time = moment(response.data.dt * 1000).format('h:mm:ss a');
+            let sunrise = moment(response.data.sys.sunrise * 1000).format('h:mm:ss a');
+            let sunset = moment(response.data.sys.sunset * 1000).format('h:mm:ss a');
+            let temp = Math.round((response.data.main.temp)-273.15);
+            let mintemp = Math.round((response.data.main.temp_min)-273.15);
+            let maxtemp = Math.round((response.data.main.temp_max)-273.15);
 
-            // document.querySelector("#icon").innerHTML = $('#').attr('src',iconurl);
-            
             document.querySelector("#MainDashboard").innerHTML = 
             `<h2>Weather in ${response.data.name}</h2>
             
-            <img src="http://openweathermap.org/img/w/" + ${response.data.weather[0].icon}+ ".png" />
-            <div>${response.data.weather[0].main}</div>
-            <div>${response.data.weather[0].description}</div>
-            <div>${response.data.weather[0].icon}</div>
+            <img src='https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png' class='weather_icon' />
+            <div><h2>Temp: ${temp}</h2></div>
+            <div><h4>Minimum Temprature: ${mintemp}</h4></div>
+            <div><h4>Maximum Temprature: ${maxtemp}</h4></div>
             <div>${response.data.name}, ${ response.data.sys.country}</div>
-            <div>${response.data.dt}</div>
-            <div>${response.data.timezone}</div>
+            <div>${date}</div>
+            <div>${time}</div>
             `
             
             document.querySelector("#WindStatus").innerHTML = 
             `<h2> Wind Status </h2>
             <div>${response.data.wind.speed} /KM</div>
             <div>${response.data.wind.deg} /Degrees</div>
-            <div>${response.data.timezone}</div>
+            <div><h3>${time}</h3></div>
             `
 
             document.querySelector("#UVIndex").innerHTML = 
             `<h2> UV Index </h2>
             <div>${response.data.main.pressure} /KM</div>
-            <div>${response.data.timezone}</div>
+            <div>${time}</div>
             `
             
             document.querySelector("#SunriseSunset").innerHTML = 
             `<h2> Sun Rise and Sun Set </h2>
-            <div>${response.data.sys.sunrise}</div>
-            <div>${response.data.sys.sunset}</div>
+            <div>${sunrise}</div>
+            <div>${sunset}</div>
             `
 
             document.querySelector("#Humidity").innerHTML = 
